a2ac4c11830955d85a9415b225a8816b08581834
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-05-02 15:51:37 +0500

    added Main Module code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css";
 import UseCallBack from "./hooks/UseCallBack";
+import UseMemo from "./hooks/UseMemo";
+import UseRef from "./hooks/UseRef";
 
 function App() {
   return (
-    <>
+    <main className="App">
       {/* example code  for useCallback */}
       <h2>useCallback Example</h2>
       <UseCallBack />
-    </>
+      <h2>useMemo Example</h2>
+      <UseMemo />
+      <h2>useRef Example</h2>
+      <UseRef />
+    </main>
   );
 }
 
