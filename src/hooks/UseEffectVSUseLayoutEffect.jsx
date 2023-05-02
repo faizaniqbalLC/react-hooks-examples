d9edbdc269d689719eda8ca93bf5366a82f2bab7
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-05-02 21:46:12 +0500

    added useFffect and useLayoutEffect

---
+++ src/hooks/UseEffectVSUseLayoutEffect.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useRef, useState } from "react";
+
+/* 
+  useEffect is asynchronous. 
+  You see the number change in the DOM before the padding changes. 
+
+  useLayoutEffect is synchronous. 
+  You see the number change only after the padding has changed.
+  */
+
+const UseEffectVSUseLayoutEffect = () => {
+  const [number, setNumber] = useState(0);
+  const [sectionStyle, setSectionStyle] = useState({});
+  const sectionRef = useRef();
+
+  // change to useLayoutEffect to see the difference
+  useEffect(() => {
+    const random = Math.floor(Math.random() * 500);
+    /* loop is just to make the changes in this example slow enough to be observable */
+    for (let i = 0; i <= 100000000; i++) {
+      if (i === 100000000) setSectionStyle({ paddingTop: `${random}px` });
+    }
+  }, [number]);
+
+  return (
+    <main className="App">
+      <section ref={sectionRef} style={sectionStyle}>
+        <p>{number}</p>
+        <button onClick={() => setNumber((prev) => prev - 1)}>-</button>
+        <button onClick={() => setNumber((prev) => prev + 1)}>+</button>
+      </section>
+    </main>
+  );
+};
+
+export default UseEffectVSUseLayoutEffect;
