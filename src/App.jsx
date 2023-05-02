c7a431fe5d3dc08bf98e93ed1cfa48f0a9fa0faf
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-05-02 21:47:17 +0500

    rendered new components of Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import "./App.css";
 import UseCallBack from "./hooks/UseCallBack";
+import UseEffectVSUseLayoutEffect from "./hooks/UseEffectVSUseLayoutEffect";
 import UseMemo from "./hooks/UseMemo";
+import UseReducer from "./hooks/UseReducer";
 import UseRef from "./hooks/UseRef";
 
 function App() {
   return (
     <main className="App">
       {/* example code  for useCallback */}
-      <h2>useCallback Example</h2>
+      <h3>useCallback Example</h3>
       <UseCallBack />
-      <h2>useMemo Example</h2>
+      <h3>useMemo Example</h3>
       <UseMemo />
-      <h2>useRef Example</h2>
+      <br />
+      <h3>useRef Example</h3>
       <UseRef />
+      <br />
+      <br />
+      <h3>useReducer Example</h3>
+      <UseReducer />
+      <br />
+      <br />
+      <h3>useEffect vs useLayoutEffect Example</h3>
+      <UseEffectVSUseLayoutEffect />
     </main>
   );
 }
