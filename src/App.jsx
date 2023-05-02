2c85c35802e83ff9de260b63c659a782766b3abd
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-05-02 13:46:38 +0500

    added useCallback hook code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import UseCallBack from "./hooks/UseCallBack";
+
+function App() {
+  return (
+    <>
+      {/* example code  for useCallback */}
+      <h2>useCallback Example</h2>
+      <UseCallBack />
+    </>
+  );
+}
+
+export default App;
---
+++ src/hooks/UseCallBack.jsx
@@ -0,0 +1,38 @@
+import { useCallback, useEffect, useState } from "react";
+
+const UseCallBack = () => {
+  const [state, setState] = useState("");
+  const [num1] = useState(5);
+  const [num2] = useState(5);
+
+  // in this case we are passing sum as a
+  // dependency to useEffect it cause to
+  // re-render whole component
+
+  // const sum = () => num1 + num2;   DON'T DO THIS ( if u pass function as a dependency to useEffect )
+
+  // wrapping sum function inside useCallback to stop re-render process
+
+  // best choice
+
+  const sum = useCallback(() => num1 + num2, [num1, num2]);
+
+  useEffect(() => {
+    console.log("useCallback-sum", sum());
+  }, [sum]);
+
+  return (
+    <>
+      <input
+        onChange={(e) => {
+          setState(e.target.value);
+        }}
+        placeholder="Change Text"
+        style={{ width: "20vh" }}
+      />
+      <h1>{state}</h1>
+    </>
+  );
+};
+
+export default UseCallBack;
